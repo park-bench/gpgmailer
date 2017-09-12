f30cc0808b8d4e17f4fb206f6d20743eb2082fcc
Andrew Klapp <andklapp@null.net>
2017-09-11 20:20:23 -0500

    Wrote test_encryption_failed test, added log messages and comments.

--- test/gpgmailbuildertest.py
+++ test/gpgmailbuildertest.py
@@ -14,6 +14,7 @@ log_level = "TRACE"
 max_operation_time = 60
 valid_signing_key_fingerprint = '32C39D741B2D0F56A57F3BD5C98DBEA2DE6613E9'
 untrusted_signing_key_fingerprint = '580F6E7B9360235DD4227A21CE428A67F602976B'
+expired_subkey_key_fingerprint = '3A227B3DA67B3EBB31DA16B117EE9CEDB09285D6'
 # All three keys use the same passphrase
 signing_key_correct_passphrase = 'lk\\4+v4*SL3r{vm^S(R";uP-l)nT+%)Ku;{0gS+"a5"1t;+6\'c]}TX4H)`c2'
 signing_key_wrong_passphrase = 'php is a great language'
@@ -37,17 +38,22 @@ class gpgmailbuildertest(unittest.TestCase):
         cls.gpgkeyring = gpgkeyring.GpgKeyRing(test_keyring_directory)
         cls.gpgmailbuilder = gpgmailbuilder.GpgMailBuilder(cls.gpgkeyring, max_operation_time)
 
+        cls.logger.info('Test class setup complete.')
+
     def setUp(self):
         self.loop_current_time = time.time()
 
     # Happy path test. We just don't want it to raise exceptions.
     def test_sign_message(self):
+        self.logger.info('Testing signature with valid key.')
         signed_message = self.gpgmailbuilder.build_signed_message(message, valid_signing_key_fingerprint,
             signing_key_correct_passphrase, self.loop_current_time)
         # TODO: Eventually, consider verifying the signature here.
 
-    # Make sure that general signature errors are covered.
-    def test_signing_failed(self):
+    # Wrong passwords are not specifically handled, so this should raise the general
+    #   SignatureError exception.
+    def test_signing_bad_password(self):
+        self.logger.info('Testing signature with bad password.')
         # should raise SignatureError
         with self.assertRaises(gpgmailbuilder.SignatureError):
             signed_message = self.gpgmailbuilder.build_signed_message(message, valid_signing_key_fingerprint,
@@ -55,6 +61,7 @@ class gpgmailbuildertest(unittest.TestCase):
 
     # Any untrusted keys should throw an exception
     def test_signing_key_untrusted(self):
+        self.logger.info('Testing signature with untrusted key.')
         # should raise GpgKeyUntrustedException
         with self.assertRaises(gpgmailbuilder.GpgKeyUntrustedException):
             signed_message = self.gpgmailbuilder.build_signed_message(message, untrusted_signing_key_fingerprint,
@@ -64,9 +71,11 @@ class gpgmailbuildertest(unittest.TestCase):
         # should raise GpgKeyExpiredException
         pass
 
+    # Subkeys are not specifically handled, so this should raise the general EncryptionError.
     def test_encryption_failed(self):
-        # just try to encrypt with expired or untrusted key
-        # should raise EncryptionError
-        pass
+        self.logger.info('Testing general encryption failure exception.')
+        with self.assertRaises(gpgmailbuilder.EncryptionError):
+            encrypted_message = self.gpgmailbuilder.build_encrypted_message(message, [expired_subkey_key_fingerprint],
+                self.loop_current_time)
 
 unittest.main()
