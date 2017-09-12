9bdb6478f7fb76ede76543465723a8fca76be5ce
Andrew Klapp <andklapp@null.net>
2017-09-11 19:36:14 -0500

    Wrote test_signing_key_untrusted and added appropriate new testing keys.

--- test/gpgmailbuildertest.py
+++ test/gpgmailbuildertest.py
@@ -12,7 +12,9 @@ import unittest
 log_file = "/dev/null"
 log_level = "TRACE"
 max_operation_time = 60
-signing_key_fingerprint = '32C39D741B2D0F56A57F3BD5C98DBEA2DE6613E9'
+valid_signing_key_fingerprint = '32C39D741B2D0F56A57F3BD5C98DBEA2DE6613E9'
+untrusted_signing_key_fingerprint = '580F6E7B9360235DD4227A21CE428A67F602976B'
+# All three keys use the same passphrase
 signing_key_correct_passphrase = 'lk\\4+v4*SL3r{vm^S(R";uP-l)nT+%)Ku;{0gS+"a5"1t;+6\'c]}TX4H)`c2'
 signing_key_wrong_passphrase = 'php is a great language'
 test_keyring_directory = './gpgmailbuilder-test-keyring'
@@ -40,7 +42,7 @@ class gpgmailbuildertest(unittest.TestCase):
 
     # Happy path test. We just don't want it to raise exceptions.
     def test_sign_message(self):
-        signed_message = self.gpgmailbuilder.build_signed_message(message, signing_key_fingerprint,
+        signed_message = self.gpgmailbuilder.build_signed_message(message, valid_signing_key_fingerprint,
             signing_key_correct_passphrase, self.loop_current_time)
         # TODO: Eventually, consider verifying the signature here.
 
@@ -48,12 +50,15 @@ class gpgmailbuildertest(unittest.TestCase):
     def test_signing_failed(self):
         # should raise SignatureError
         with self.assertRaises(gpgmailbuilder.SignatureError):
-            signed_message = self.gpgmailbuilder.build_signed_message(message, signing_key_fingerprint,
+            signed_message = self.gpgmailbuilder.build_signed_message(message, valid_signing_key_fingerprint,
                 signing_key_wrong_passphrase, self.loop_current_time)
 
+    # Any untrusted keys should throw an exception
     def test_signing_key_untrusted(self):
         # should raise GpgKeyUntrustedException
-        pass
+        with self.assertRaises(gpgmailbuilder.GpgKeyUntrustedException):
+            signed_message = self.gpgmailbuilder.build_signed_message(message, untrusted_signing_key_fingerprint,
+                signing_key_correct_passphrase, self.loop_current_time)
 
     def test_signing_key_expired(self):
         # should raise GpgKeyExpiredException
