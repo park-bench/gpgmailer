76ada1d6aea374a8476a906ac5f84d306876e9a1
Emily Frost <emfrost@posteo.net>
2019-07-29 21:57:23 -0500

    Implemented exception chaining in previously marked places.

--- src/usr/share/gpgmailer/gpgmailerd.py
+++ src/usr/share/gpgmailer/gpgmailerd.py
@@ -74,17 +74,16 @@ def get_user_and_group_ids():
     try:
         program_user = pwd.getpwnam(PROCESS_USERNAME)
     except KeyError as key_error:
-        # TODO: When switching to Python 3, convert to chained exception. (issue 15)
-        print('User %s does not exist. %s: %s' % (
-            PROCESS_USERNAME, type(key_error).__name__, str(key_error)))
-        raise key_error
+        message = 'User %s does not exist. %s: %s' % (
+            PROCESS_USERNAME, type(key_error).__name__, str(key_error))
+        print(message)
+        raise InitializationException(message) from key_error
     try:
         program_group = grp.getgrnam(PROCESS_GROUP_NAME)
     except KeyError as key_error:
-        # TODO: When switching to Python 3, convert to chained exception. (issue 15)
-        print('Group %s does not exist. %s: %s' % (
-            PROCESS_GROUP_NAME, type(key_error).__name__, str(key_error)))
-        raise key_error
+        message = 'Group %s does not exist. %s: %s' % (
+            PROCESS_GROUP_NAME, type(key_error).__name__, str(key_error))
+        raise InitializationException(message) from key_error
 
     return program_user.pw_uid, program_group.gr_gid
 
